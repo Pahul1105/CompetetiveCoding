d03450edf1e3314a6d27523934872db7604f6f7c
Pahul1105 <11pahulsingh@gmail.com>
2026-02-05 22:30:53 +0530

    Update 2047B.cpp

--- 24BCS10261_Pahulpreet_Singh_616B/experiment_2/2047B.cpp
+++ 24BCS10261_Pahulpreet_Singh_616B/experiment_2/2047B.cpp
@@ -1,1 +1,42 @@
-
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (n == 1) {
+        cout << s << '\n';
+        return;
+    }
+    vector<int> cnt(26);
+    for (char ch : s) {
+        cnt[ch - 'a']++;
+    }
+    char best = s[0];
+    int bestCnt = cnt[best - 'a'];
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i] > bestCnt) {
+            bestCnt = cnt[i];
+            best = char('a' + i);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (s[i] != best) {
+            s[i] = best;
+            break;
+        }
+    }
+    cout << s << '\n';
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
