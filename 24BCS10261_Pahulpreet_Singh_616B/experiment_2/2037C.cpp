50548c88006c0d1cc263b7f1895482d82189ec53
Pahul1105 <11pahulsingh@gmail.com>
2026-02-05 22:32:42 +0530

    Implement main function for number processing

--- 24BCS10261_Pahulpreet_Singh_616B/experiment_2/2037C.cpp
+++ 24BCS10261_Pahulpreet_Singh_616B/experiment_2/2037C.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int tc;
+    cin >> tc;
+    while(tc--) {
+        int n;
+        cin >> n;
+        if (n == 2 || n == 3) {
+            cout << -1 << "\n"; 
+            continue;
+        }
+        if (n == 4) {
+            cout << "3 1 4 2\n"; 
+            continue;
+        }
+        for (int i = n; i >= 1; i--) {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
