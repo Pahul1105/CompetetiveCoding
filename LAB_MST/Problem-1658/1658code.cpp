d12b9f9b180321f1a149cee49c8e56abf5364648
Pahul1105 <11pahulsingh@gmail.com>
2026-03-12 21:36:16 +0530

    Update 1658code.cpp

--- LAB_MST/Problem-1658/1658code.cpp
+++ LAB_MST/Problem-1658/1658code.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        int total= 0;
+        for(int num : nums){
+            total+=num;
+        }
+        int target=total-x;
+        if(target < 0)
+            return -1;
+        int left=0;
+        int sum=0;
+        int len=-1;
+        for(int right=0;right<n;right++){
+            sum+=nums[right];
+            while(sum > target){
+                sum-=nums[left];
+                left++;
+            }
+            if(sum==target){
+                len = max(len,right-left+1);
+            }
+        }
+        if(len==-1)
+            return -1;
+        return n-len;
+    }
+};
