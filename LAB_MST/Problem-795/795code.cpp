45d754a0ce9c5aed301fd915ffaeaa949805b1f6
Pahul1105 <11pahulsingh@gmail.com>
2026-03-12 21:23:46 +0530

    Implement Solution class for bounded subarray count

--- LAB_MST/Problem-795/795code.cpp
+++ LAB_MST/Problem-795/795code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int count(vector<int>& nums, int k){
+        int len=0;
+        int total=0;
+        for(int x:nums){
+            if(x<=k)
+                len++;
+            else
+                len=0;
+            total+=len;
+        }
+        return total;
+    }
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        return count(nums,right)-count(nums,left-1);
+    }
+};
